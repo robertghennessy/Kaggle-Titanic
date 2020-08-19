b4cfe5ae80204f29fd6fc3f4132f4e079f815d88
Robert Hennessy <roberthennessy@roberts-mbp.lan>
2020-08-18 17:33:21 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, test')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
